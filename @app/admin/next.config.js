52b54efb836b7b712c74eb75f1b2d6a9a4b9003b
Anthony Shew <anthonyshew@gmail.com>
2024-01-19 16:18:34 -0700

    Commenting.

--- @app/admin/next.config.js
+++ @app/admin/next.config.js
@@ -2,7 +2,12 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@cms/packages/ui', '@cms/packages/templates'],
+  transpilePackages: [
+    '@cms/ui',
+    // You would update this to @cms/templates similar to @cms/ui
+    // once it is turned into a proper workspace.
+    '@cms/packages/templates',
+  ],
   webpack(config) {
     config.resolve.alias = {
       ...config.resolve.alias,
