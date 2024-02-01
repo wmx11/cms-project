82d6b213547b7fde8923ad93eece9ce16a41198b
Vainius <vmykolaitis1@gmail.com>
2024-02-01 10:36:06 +0200

    Tweaked scaling values

--- packages/template-engine/constants/scaling.ts
+++ packages/template-engine/constants/scaling.ts
@@ -1,6 +1,6 @@
 /**
  * @description constants used to handle scaling (canvas scaling)
  */
-export const SCALE_INTENSITY = 0.055;
+export const SCALE_INTENSITY = 0.025;
 export const SCALE_MAX = 2;
-export const SCALE_MIN = SCALE_INTENSITY;
+export const SCALE_MIN = 0.2;
