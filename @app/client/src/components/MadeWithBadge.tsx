f6391ff54491fa9b8df5ffbf7b7d2a0190be91fb
Vainius <vmykolaitis1@gmail.com>
2024-06-17 13:21:42 +0300

    Added PROJECT_URL

--- @app/client/src/components/MadeWithBadge.tsx
+++ @app/client/src/components/MadeWithBadge.tsx
@@ -1,12 +1,12 @@
-import { Badge } from '@cms/ui/components/Badge';
 import TigleeLogo from '@cms/assets/tiglee-logo.png';
-import Link from 'next/link';
-import React from 'react';
+import { PROJECT_URL } from '@cms/tiglee-engine/constants';
+import { Badge } from '@cms/ui/components/Badge';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const MadeWithBadge = () => {
   return (
-    <Link href="https://tiglee.io" target="_blank">
+    <Link href={`https://${PROJECT_URL}`} target="_blank">
       <Badge className="flex gap-2 bg-gradient-to-r from-violet-600 to-purple-500">
         <Image src={TigleeLogo} width={15} alt="Tiglee logo" />
         <span>Made in Tiglee</span>
